f065cf083cd58779273cfa4207c774d40cf8d8c5
cholid.dnr <cholid.dnr@gmail.com>
2025-06-01 07:51:46 +0700

    init setup

---
+++ content/index.md
@@ -0,0 +1,8 @@
+---
+title: "Title of the page"
+description: "meta description of the page"
+---
+
+# My First Page
+
+Here is some content.
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/content", "@nuxt/eslint"],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+});
