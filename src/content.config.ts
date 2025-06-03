d6722fc380e004be1fcc787c8f0af1b9fb8baa0a
cholid.dnr <cholid.dnr@gmail.com>
2025-06-03 17:12:08 +0700

    add profile and project content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,28 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const project = defineCollection({
+  // Load Markdown and MDX files in the `src/content/project/` directory.
+  loader: glob({ base: "./src/content/project", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    date: z.coerce.date(),
+  }),
 });
 
-export const collections = { blog };
+const profile = defineCollection({
+  // Load Markdown and MDX files in the `src/content/profile/` directory.
+  loader: glob({ base: "./src/content/profile", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    date: z.coerce.date(),
+  }),
+});
+
+export const collections = { project, profile };
